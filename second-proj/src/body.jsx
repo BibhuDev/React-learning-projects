298bbc3ed892989aea17656ae6b21eb68d17f4c2
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-05-24 23:49:22 +0530

    added components

---
+++ second-proj/src/body.jsx
@@ -0,0 +1,16 @@
+function Body(){
+    return <div class="container text-center">
+  <div class="row justify-content-md-center">
+    <div class="col-md-auto">
+      Variable width content
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-auto">
+      Variable width content
+    </div>
+  </div>
+</div>
+}
+
+export default Body;
--- second-proj/src/head.jsx
+++ second-proj/src/head.jsx
@@ -1,5 +1,12 @@
-function Head(){
-    return <h1>Bharat clock</h1>
+function Head() {
+    return <div class="grid text-center">
+
+        <div class="g-col-4">
+            <h2>Bharat Clock</h2>
+        </div>
+
+    </div>
+
 }
 
 export default Head;
