6ca97e56bbc81dd5d43ddbcf5fff00206ed40a93
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-06-17 16:50:48 +0530

    initialising this project

---
+++ To-do app/src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Header } from './components/Header'
+import { Tabs } from './components/Tabs'
+import { TodoCard } from './components/TodoCard'
+import { TodoInput } from './components/TodoInput'
+import { TodoList } from './components/TodoList'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <Tabs />
+      <TodoCard />
+      <TodoInput />
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
